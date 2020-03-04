dee49d8b38122d6116f0743f424069754f5d5a02
sine <963366202@qq.com>
2020-03-04 14:01:29 +0800

    fix regular expression

--- pptx2md/tools.py
+++ pptx2md/tools.py
@@ -4,7 +4,7 @@ def fix_null_rels(file_path):
     temp_dir_name = tempfile.mkdtemp()
     shutil.unpack_archive(file_path, temp_dir_name, 'zip')
     rels = [os.path.join(dp, f) for dp, dn, filenames in os.walk(temp_dir_name) for f in filenames if os.path.splitext(f)[1] == '.rels']
-    pat = re.compile(r'<\S*Relationship[^>]+Target\S*=\S*"NULL"\S*/>', re.I)
+    pat = re.compile(r'<\S*Relationship[^>]+Target\S*=\S*"NULL"[^>]*/>', re.I)
     for fn in rels:
         f = open(fn, 'r+')
         content = f.read()
